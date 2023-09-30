d4e8de5cdfadec6cd40c18504f46e8c48aea7107
Natarajan <nataraj.itsolution@gmail.com>
2023-09-30 23:10:00 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCrud.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(string code)
+        {
+            ViewData["editcode"] = code;
+            return View();
+        }
+    }
+}
